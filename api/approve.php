bb35cb2cf7ef2d11dcfe2a5847b17681b1383279
Ridho-lang28 <ridhoaskuri28@student.uns.ac.id>
2026-04-27 00:38:25 +0700

    Update approve.php

--- api/approve.php
+++ api/approve.php
@@ -8,4 +8,17 @@ mysqli_query($conn,"UPDATE kuliner SET status='approved' WHERE id='$id'");
 // ubah status jadi approved
 
 header("Location: index.php?page=admin"); 
-// redirect balik ke halaman admin
+// redirect balik ke halaman admin<?php
+include "koneksi.php";
+
+if(!isset($_GET['id'])){
+  die("ID tidak ditemukan");
+}
+
+$id = intval($_GET['id']);
+
+mysqli_query($conn,"UPDATE kuliner SET status='approved' WHERE id=$id");
+
+header("Location: /");
+exit;
+?>
