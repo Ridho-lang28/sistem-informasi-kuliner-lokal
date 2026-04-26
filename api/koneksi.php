70cd21b9ce54a076af56de04fec4309effab2401
Ridho-lang28 <ridhoaskuri28@student.uns.ac.id>
2026-04-27 00:52:21 +0700

    Update koneksi.php

--- api/koneksi.php
+++ api/koneksi.php
@@ -1,11 +1,25 @@
 <?php
 $host = "gateway01.ap-southeast-1.prod.alicloud.tidbcloud.com";
-$port = 4000;
 $user = "3KTgRBgr6VCylbp.root";
 $pass = "ns5fTU2zrgVgdkvg";
 $db   = "KulinerLokal";
+$port = 4000;
 
-$conn = mysqli_connect($host, $user, $pass, $db, $port);
+$conn = mysqli_init();
+
+// WAJIB untuk TiDB (SSL)
+mysqli_ssl_set($conn, NULL, NULL, NULL, NULL, NULL);
+
+mysqli_real_connect(
+    $conn,
+    $host,
+    $user,
+    $pass,
+    $db,
+    $port,
+    NULL,
+    MYSQLI_CLIENT_SSL
+);
 
 if (!$conn) {
     die("Koneksi gagal: " . mysqli_connect_error());
