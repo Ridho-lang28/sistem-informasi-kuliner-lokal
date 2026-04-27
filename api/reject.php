b2e4c893eb7b541caf07e511eadb0dc8276d40a1
Ridho-lang28 <ridhoaskuri28@student.uns.ac.id>
2026-04-27 17:46:41 +0700

    Update reject.php

--- api/reject.php
+++ api/reject.php
@@ -1,17 +1,9 @@
 <?php
 include "koneksi.php";
 
-$nama = $_POST['nama_makanan'];
-$kategori = $_POST['kategori'];
-$lokasi = $_POST['lokasi'];
-$harga = $_POST['harga'];
-$deskripsi = $_POST['deskripsi'];
-$rating = $_POST['rating'];
+$id = $_GET['id'];
 
-mysqli_query($conn,"INSERT INTO kuliner 
-(nama_makanan,kategori,lokasi,harga,deskripsi,rating,status)
-VALUES 
-('$nama','$kategori','$lokasi','$harga','$deskripsi','$rating','pending')");
+mysqli_query($conn, "DELETE FROM kuliner WHERE id='$id'");
 
-echo "OK";
+header("Location: ../admin.php");
 ?>
